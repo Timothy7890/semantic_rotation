656bfd558d28b1c9fc76c7987a1a9e07ad1e3a9c
Administrator <1434070486@qq.com>
2024-06-22 22:41:40 +0800

    修改路径表达

--- Pipeline/Run_Sematic_R.py
+++ Pipeline/Run_Sematic_R.py
@@ -2,12 +2,14 @@ import os
 from Tran_and_scaling.Reput_txt import translate_and_scale_point_cloud
 from Vedio.Semantic_vedio import generate_rotating_point_cloud_video_from_txt
 
+workspace = r"C:\Users\Administrator\Desktop\mp4\A-input"
 # 使用示例
-input_txt_dir = r"C:\Users\Administrator\Desktop\mp4\other_input\input"
-output_dir = r"C:\Users\Administrator\Desktop\mp4\other_input\combine_txt"
-output_mp4_dir = rf"C:\Users\Administrator\Desktop\mp4\other_output"
+input_txt_dir = os.path.join(workspace, "raw_txt")
+output_dir = os.path.join(workspace, "combine_txt")
+output_mp4_dir = os.path.join(workspace, "output")
 
-OVERWRITE = False
+OVERWRITE = True
+reput_size = 0.75
 
 # 创建输出目录（如果不存在）
 os.makedirs(output_dir, exist_ok=True)
@@ -18,7 +20,7 @@ for filename in os.listdir(input_txt_dir):
         # 构建输入文件路径
         input_file = os.path.join(input_txt_dir, filename)
         # 调用函数处理点云文件
-        translate_and_scale_point_cloud(input_file, output_dir, OVERWRITE)
+        translate_and_scale_point_cloud(input_file, output_dir, reput_size, OVERWRITE)
         # 构建处理后的txt文件路径
         processed_txt = os.path.join(output_dir, filename)
         # 调用函数生成旋转点云视频
